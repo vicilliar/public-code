536a284d822bc9307b137f813bc2a07f89ece08c
joshuak <joshua@marqo.ai>
2023-05-16 18:08:58 +0800

    ,

--- preload_models/index_custom.py
+++ preload_models/index_custom.py
@@ -6,7 +6,7 @@ settings = {
             "name": "ViT-B-32-quickgelu",
                 "dimensions": 512,
                 "url": "https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_b_32-quickgelu-laion400m_avg-8a00ab3c.pt",
-                "type": "open_clip",
+                "type": "open_clip"
             },
         "normalize_embeddings": True,
     },
