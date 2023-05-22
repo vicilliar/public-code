b9fa83e2f3b20438da5893e0143f930cfe75d119
joshuak <joshua@marqo.ai>
2023-05-22 19:52:14 +0800

    update script

---
+++ start_stop/restart_util.py
@@ -0,0 +1,54 @@
+import logging
+import subprocess
+import os
+import time
+from marqo import Client
+import pprint
+import json
+
+def rerun_marqo_with_env_vars(env_vars: str = ""):
+    """
+        Given a string of env vars, stop and rerun Marqo using the start script appropriate
+        for the current test config
+
+        Returns the console output of all the subprocess calls
+    """
+
+    run_process = subprocess.Popen([
+        "bash",                             # command: run
+        "run_marqo_cuda.sh",                # script to run
+        "marqo_docker_0",                   # arg $1 in script
+        env_vars                            # arg $2 in script
+        ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    
+    # Read and print the output line by line (in real time)
+    for line in run_process.stdout:
+        print(line, end='')
+    
+    # Wait for the process to complete
+    run_process.wait()
+    return f"{output_1}\n{run_process.stdout}"
+
+def test_run():
+    # Rerun marqo with new custom model
+    open_clip_model_object = {
+        "model": "open-clip-1",
+        "model_properties": {
+            "name": "ViT-B-32-quickgelu",
+            "dimensions": 512,
+            "type": "open_clip",
+            "url": "https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_b_32-quickgelu-laion400m_avg-8a00ab3c.pt"
+        }
+    }
+
+    print(f"Attempting to rerun marqo with custom model {open_clip_model_object['model']}")
+    utilities.rerun_marqo_with_env_vars(
+        env_vars = f"-e MARQO_MODELS_TO_PRELOAD=[{json.dumps(open_clip_model_object)}]"
+    )
+
+    # check preloaded models (should be custom model)
+    custom_models = ["open-clip-1"]
+    res = self.client.get_loaded_models()
+    assert set([item["model_name"] for item in res["models"]]) == set(custom_models)
+
+test_run()
---
+++ start_stop/run_marqo_cuda.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# args:
+# $1 : marqo_image_name - name of the image you want to test
+# $2 : env_vars - string representing the env vars to pass marqo
+set -x
+docker rm -f marqo;
+
+# docker call args array
+# declare -a args
+#args=("--name" "marqo" "--gpus" "all" "--privileged" "-p" "8882:8882" "--add-host" "host.docker.internal:host-gateway")
+
+# add $2 env vars if it's set
+#if [ -n "$2" ]; then
+#    echo "checking arg2: $2"
+#    # split $2 into an array on spaces
+#    IFS=' ' read -r -a env_vars <<< "$2"
+#    # add each item in env_vars to args
+#    for var in "${env_vars[@]}"; do
+#        args+=("$var")
+#    done
+#fi
+
+# add $1 marqo image name to args
+#args+=("$1")
+#docker run -d "${args[@]}"
+
+# TODO put this back (remove models to preload)
+docker run --name marqo --gpus all --privileged -p 8882:8882 --add-host host.docker.internal:host-gateway ${2:+$2} "$1"
+
+# Follow the logs of the Docker container in the background and capture its PID
+docker logs -f marqo &
+LOGS_PID=$!
+
+# wait for marqo to start
+set +x
+until [[ $(curl -v --silent --insecure http://localhost:8882 2>&1 | grep Marqo) ]]; do
+    sleep 0.1;
+done;
+
+# Kill the `docker logs` command (so subprocess does not wait for it)
+kill $LOGS_PID
