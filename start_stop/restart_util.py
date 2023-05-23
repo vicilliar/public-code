84769a48bcc4b25317db6682c2b1c0f65304a236
joshuak <joshua@marqo.ai>
2023-05-23 15:31:33 +0800

    update

--- start_stop/restart_util.py
+++ start_stop/restart_util.py
@@ -51,7 +51,7 @@ def test_run():
         env_vars = [
             '-e', f"MARQO_MODELS_TO_PRELOAD=[{json.dumps(open_clip_model_object)}]",
             '-e', f"MARQO_MAX_NUMBER_OF_REPLICAS=5",
-            '-e', f'MARQO_LOG_LEVEL="debug"',
+            '-e', f'MARQO_LOG_LEVEL=debug',
             '-e', f'MARQO_ENABLE_THROTTLING=TRUE'
         ]
     )
